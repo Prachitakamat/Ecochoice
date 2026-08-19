d4fe7829395235331239c16390b3832f1c7ad4da
Prachitakamat <155889913+Prachitakamat@users.noreply.github.com>
2026-08-19 17:40:40 +0530

    Delete trial.py

--- trial.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("cleaning.csv", newline="", encoding="utf-8") as f:
-    reader = csv.reader(f)
-
-    for row_num, row in enumerate(reader, 1):
-        if len(row) != 8:
-            print("PROBLEM:", row_num, "has", len(row), "columns")
-            print(row)
