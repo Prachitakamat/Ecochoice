59ffa23fc64a863a40b2484ac370b4165b0f039c
Prachitakamat <155889913+Prachitakamat@users.noreply.github.com>
2026-08-19 17:37:07 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,53 @@
+import csv
+import mysql.connector
+
+conn=mysql.connector.connect(host="localhost",user="root",password="prachkamatroot")
+cursor=conn.cursor()
+cursor.execute('create database if not exists EcoChoice')
+cursor.execute('use EcoChoice')
+
+def create_table(table_name):
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    query = f"""CREATE TABLE IF NOT EXISTS {table_name}(
+           id BIGINT UNSIGNED PRIMARY KEY,
+           name VARCHAR(100) ,
+           manufacturer VARCHAR(100) ,
+           price DECIMAL(10, 2) , 
+           quantity Varchar(100) DEFAULT '0',
+           description VARCHAR(250),
+           certifications VARCHAR(250),
+           image1 VARCHAR(700),
+           link VARCHAR(800) default null,
+           rating decimal(10,2) default null
+           );
+           """
+    cursor.execute(query)
+    values = ''
+    insert_query = f"insert into {table_name} (id,name,manufacturer, price, quantity, description, certifications, image1, link) values {values};"
+
+    csv_file = table_name+'.csv'
+    file_path='./data_files/'+csv_file
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            headers = next(reader)
+            for row in reader:
+                values = tuple(row)
+                insert_query = f"insert into {table_name} (id,name,manufacturer, price, quantity, description, certifications, image1, link) values {values};"
+
+                cursor.execute(insert_query)
+        conn.commit()
+
+    except FileNotFoundError:
+        print('File not found')
+
+category_dict={1:'laundry',2:'cleaning', 3:'misc_household', 4:'stationery', 5:'self_care', 6:'health'}
+for table_name in category_dict.values():
+    create_table(table_name)
+
+
+
+
+cursor.close()
+conn.close()
+
---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, jsonify, session
+
+import product_display as p
+
+app = Flask(__name__)
+app.secret_key='secret_key#!.4357810'
+
+@app.route('/')
+def home():
+    print('home')
+    return render_template('index.html')
+
+product_selection=[]
+@app.route('/selected-product', methods=['POST'])
+def get_selected_text():
+    print("received post")
+    product=request.get_json().get('product_name')
+    category=request.get_json().get('category_name')
+    session['product']=product
+    session['category']=category
+    return jsonify({"status": "success", "redirect_url": "/results-page"})
+
+
+@app.route('/results-page')
+def display_products():
+    product_category=session.get('product')
+    category=session.get('category')
+    product_data={}
+    product_data[product_category]=p.products(category,product_category)
+    print(product_data)
+
+    return render_template('products.html', categories=product_data)
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
